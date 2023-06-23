f9059a28f06c97f5d6fe566bfb984ad2fcca6b60
Hasnah Insita Sahla <113223325+hasnah2@users.noreply.github.com>
2023-06-23 14:53:22 +0700

    Add files via upload

---
+++ OPREC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hasnah.oprec;
+
+/**
+ *
+ * @author hasna
+ */
+public class OPREC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
